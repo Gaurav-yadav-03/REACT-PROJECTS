0515276f79178cbc21750538574d6d0d426d7542
Gaurav-Yadav-03 <igaurav334@gmail.com>
2024-04-27 21:02:13 +0530

    react

--- DiceRoll/vite.config.js
+++ DiceRoll/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: ' https://gaurav-yadav-03.github.io/REACT-PROJECTS/',
 })
